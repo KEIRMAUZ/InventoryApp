d7d1c91d4019c93baa2f0de70d4f47db39ccae20
KEIRMAUZ <kevinmanuelarteagaruiz@gmail.com>
2025-11-19 07:43:31 -0600

    [ADD] CRUD terminado

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [ProductsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
   TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'user',
-    password: 'password',
-    database: 'camera_app',
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    synchronize: true,
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
   })
   ],
   controllers: [AppController],
